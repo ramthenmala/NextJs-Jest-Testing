21dab55fd88627593f35e23997f5605a28150c38
Ram kumar <webbieram@outlook.com>
2022-07-16 00:52:16 +0530

    prettier commit

--- src/components/Hello.spec.tsx
+++ src/components/Hello.spec.tsx
@@ -1,10 +1,7 @@
-import {render, screen} from '@testing-library/react'
-import Hello from './Hello';
-
-describe('Testing Hello Component', () => {
-    it('Should return string Hello World', () => {
-        render(<Hello/>)
-        const helloText = screen.getByText('Hello world');
-        expect(helloText).toBeInTheDocument()
-    })
-})
+import { render, screen } from "@testing-library/react";
+import Hello from "./Hello";
+it("Should return string Hello World", () => {
+  render(<Hello />);
+  const helloText = screen.getByText("Hello world");
+  expect(helloText).toBeInTheDocument();
+});
--- src/components/sum.spec.ts
+++ src/components/sum.spec.ts
@@ -1,8 +1,6 @@
-import { sum } from './sum';
+import { sum } from "./sum";
 
-describe('Sum testing', ()=> {
-    it('should return 7', ()=> {
-        const a: number = 7;
-        expect(sum(5, 2)).toBe(a)
-    })
-})
+it("should return 7", () => {
+  const a: number = 7;
+  expect(sum(5, 2)).toBe(a);
+});
